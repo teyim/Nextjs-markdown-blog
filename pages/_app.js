07cf2ee695882eff95cbe6ff9a757193b227ed18
teyim <teyimasobo@gmail.com>
2022-03-06 20:10:45 +0200

    Modify _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Layout from '../components/layout/layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
