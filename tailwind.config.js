d028b752b38b7571d9f823279dc5a31681bc3604
teyim <teyimasobo@gmail.com>
2022-03-07 17:04:26 +0200

    Add dropdown animation to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,31 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       fontFamily: {
         "Rubik": ["Rubik"],
         "Bai": ["Bai Jamjuree"],
+      },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.2s ease-out'
       }
     },
   },
-  plugins: [],
+  plugins: [
+  ],
 }
