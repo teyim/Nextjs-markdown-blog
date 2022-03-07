c86ed4820e01eacdba65806064ac0bae283e0e58
teyim <teyimasobo@gmail.com>
2022-03-07 17:20:01 +0200

    Add dropdown to navigation

--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -1,25 +1,61 @@
 import Link from 'next/link'
 import React, { useState } from 'react';
+import { AiOutlineTwitter, AiOutlineGithub } from 'react-icons/ai'
 
-const Navbar = ({ menuOpen, setMenuOpen }) => (
+
+const Navbar = ({ menuOpen, setMenuOpen }) => {
+    const [showDropDown, setshowDropDown] = useState(false)
+    const toggleDropDown = () => {
+        setshowDropDown(!showDropDown)
+    }
+    return (
     <nav className="flex justify-between align-middle text-lg ">
-        <Link href='/' passHref><h2 className="font-Bai font-extrabold text-2xl">Teyim A.</h2></Link>
+            <span className="font-Bai font-extrabold text-2xl"><Link href='/' passHref>Teyim A.</Link></span>
         {!menuOpen && (<>
             <ul className=" justify-evenly w-2/4 my-auto hidden md:flex" >
+                    <li>
                 <span className=" hover:font-semibold hover:underline  text-xl font-medium text-gray-800"><Link href="/" >All posts</Link></span>
-                <span className=" hover:font-semibold hover:underline text-xl font-medium text-gray-800"><Link href="/" >Topics</Link></span>
+                    </li>
+                    <li>
+                        <button className="hover:font-semibold  text-xl font-medium text-gray-800 px-4 text-center inline-flex items-center " type="button" onClick={toggleDropDown} >Topics<svg className="w-4 h-4 ml-2 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLineCap="round" strokeLineJoin="round" strokeWidth="3" d="M19 9l-7 7-7-7"></path></svg></button>
+                        {showDropDown && (
+                            <div className=" font-semibold text-gray-600 absolute bg-white  z-50 list-none divide-y divide-gray-100 rounded-md w-[200px]  shadow-xl my-4 animate-fade-in-down" >
+                                <ul className="py-1" >
+                                    <li>
+
+                                        <span className=" text-lg hover:bg-gray-100  block px-6 py-2"><Link href="/" >React js </Link></span>
+                                    </li>
+                                    <li>
+                                        <span className=" text-lg hover:bg-gray-100  block px-6 py-2"><Link href="/" >React Native</Link></span>
+                                    </li>
+                                    <li>
+                                        <span className=" text-lg hover:bg-gray-100  block px-6 py-2"><Link href="/" >Vanilla js</Link></span>
+                                    </li>
+                                    <li>
+                                        <span className=" text-lg hover:bg-gray-100 block px-6 py-2"><Link href="/" >Next js</Link></span>
+                                    </li>
+                                    <li>
+                                        <span className=" text-lg hover:bg-gray-100  block px-6 py-2"><Link href="/" >Typescript</Link></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
+                    </li>
+                    <li>
                 <span className=" hover:font-semibold hover:underline text-xl font-medium text-gray-800"><Link href="/" >About</Link></span>
+                    </li>
             </ul>
             <div className="justify-evenly w-1/6 my-auto hidden md:flex">
-                <span className=" hover:font-semibold hover:underline text-xl font-medium text-gray-800"><Link href="/" >Twitter</Link></span>
-                <span className=" hover:font-semibold hover:underline text-xl font-medium text-gray-800"><Link href="/" >Github</Link></span>
+                    <span className=" hover:font-semibold hover:underline text-xl font-medium text-gray-800"><Link href="/" passHref><a><AiOutlineTwitter className='w-7 h-7 my-auto mx-1' /></a></Link></span>
+                    <span className=" hover:font-semibold hover:underline text-xl font-medium text-gray-800"><Link href="/" passHref><a><AiOutlineGithub className='w-7 h-7 my-auto mx-1' /></a></Link></span>
             </div>
         </>)
         }
         <button type="button" aria-label="Toggle mobile menu" onClick={() => setMenuOpen(!menuOpen)} className="rounded md:hidden focus:outline-none focus:ring focus:ring-gray-500 focus:ring-opacity-50"><MenuAlt4Svg menuOpen={menuOpen} /></button>
     </nav>
 
-);
+    )
+};
 
 const MobileMenu = ({ children }) => (
     <nav className=" text-center flex flex-col space-y-3 md:hidden bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200">
@@ -37,18 +73,51 @@ const MenuAlt4Svg = ({ menuOpen }) => (
 
 const Navigation = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [showDropDown, setshowDropDown] = useState(false)
+    const toggleDropDown = () => {
+        setshowDropDown(!showDropDown)
+    }
     return (
         <div className='top-0 z-10 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 px-8 py-4 font-Bai w-full md:py-8 md:px-14  '>
             <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
             {menuOpen &&
                 <MobileMenu>
-                    <span className=" hover:text-black hover:underline focus:underline text-xl mt-4 font-bold text-gray-700"><Link href="/" >All posts</Link></span>
-                    <span className=" hover:text-black hover:underline focus:underline text-xl font-bold text-gray-700"><Link href="/" >Topics</Link></span>
+                    <ul className='list-none '>
+                        <li className='my-4'>
+                            <span className=" hover:text-black hover:underline focus:underline text-xl font-bold text-gray-700"><Link href="/" >All posts</Link></span>
+                        </li>
+                        <li className='my-4'>
+                            <button className="font-bold  text-xl  text-gray-800 px-8 text-center inline-flex items-center " type="button" onClick={toggleDropDown} >Topics<svg className="w-4 h-4 ml-2 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLineCap="round" strokeLineJoin="round" strokeWidth="3" d="M19 9l-7 7-7-7"></path></svg></button>
+                            {showDropDown && (
+                                <div className="font-semibold text-gray-600 z-50 list-none divide-y divide-gray-100 rounded-md w-full  my-4 animate-fade-in-down" >
+                                    <ul className="py-1" >
+                                        <li>
+                                            <span className=" text-lg hover:bg-gray-100  block px-6 py-2"><Link href="/" >React js </Link></span>
+                                        </li>
+                                        <li>
+                                            <span className=" text-lg hover:bg-gray-100  block px-6 py-2"><Link href="/" >React Native</Link></span>
+                                        </li>
+                                        <li>
+                                            <span className=" text-lg hover:bg-gray-100  block px-6 py-2"><Link href="/" >Vanilla js</Link></span>
+                                        </li>
+                                        <li>
+                                            <span className=" text-lg hover:bg-gray-100  block px-6 py-2"><Link href="/" >Next js</Link></span>
+                                        </li>
+                                        <li>
+                                            <span className=" text-lg hover:bg-gray-100 text-gray-700 block px-6 py-2"><Link href="/" >Typescript</Link></span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            )}
+                        </li>
+                        <li className='my-4'>
                     <span className=" hover:text-black hover:underline focus:underline text-xl font-bold text-gray-700"><Link href="/" >About</Link></span>
+                        </li>
+                    </ul>
                     <hr className='text-black border border-gray-800' />
-                    <div className="flex flex-col  w-1/6 my-auto  mx-auto">
-                        <span className="  hover:text-black hover:underline focus:underline text-lg font-bold text-gray-700"><Link href="/" >Twitter</Link></span>
-                        <span className="  hover:text-black hover:underline focus:underline text-lg font-bold text-gray-700"><Link href="/" >Github</Link></span>
+                    <div className="flex justify-evenly w-1/4 md:w-1/6 my-auto  mx-auto">
+                        <span className=" hover:font-semibold hover:underline text-xl font-medium text-gray-800"><Link href="/" passHref><a><AiOutlineTwitter className='w-7 h-7 my-auto mx-1' /></a></Link></span>
+                        <span className=" hover:font-semibold hover:underline text-xl font-medium text-gray-800"><Link href="/" passHref><a><AiOutlineGithub className='w-7 h-7 my-auto mx-1' /></a></Link></span>
                     </div>
                 </MobileMenu>}
         </div>
