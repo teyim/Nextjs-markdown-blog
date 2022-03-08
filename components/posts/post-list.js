2e5e67c9aa96278fc3c300ebe3996fda53f5fd8c
teyim <teyimasobo@gmail.com>
2022-03-09 00:26:25 +0200

    Modify post-list component

---
+++ components/posts/post-list.js
@@ -0,0 +1,24 @@
+import PostCard from "./post-card"
+
+function PostList({ posts }) {
+    return (
+
+        <div className="w-full font-Rubik text-gray-800 ">
+
+            <div className="max-w-screen-2xl flex flex-wrap  overflow-hidden  justify-evenly  p-8">
+                <div className="w-full md:py-6 md:px-14">
+                    <h1 className="font-Bai text-3xl font-bold text-center md:text-left my-2 md:my-0 md:text-2xl ">All Posts</h1>
+                </div>
+
+                {posts.map(post => (
+                    <PostCard key={post.slug} post={post} />
+                ))}
+
+            </div>
+        </div>
+
+
+    )
+}
+
+export default PostList
