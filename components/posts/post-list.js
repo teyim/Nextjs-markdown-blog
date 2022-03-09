cfda94c78082a118dcb9d84020f7df4cef8e9965
teyim <teyimasobo@gmail.com>
2022-03-09 20:45:28 +0200

    Modify post-list component

--- components/posts/post-list.js
+++ components/posts/post-list.js
@@ -1,13 +1,13 @@
 import PostCard from "./post-card"
 
-function PostList({ posts }) {
+function PostList({ posts, topic }) {
     return (
 
         <div className="w-full font-Rubik text-gray-800 ">
 
             <div className="max-w-screen-2xl flex flex-wrap  overflow-hidden  justify-evenly  p-8">
                 <div className="w-full md:py-6 md:px-14">
-                    <h1 className="font-Bai text-3xl font-bold text-center md:text-left my-2 md:my-0 md:text-2xl ">All Posts</h1>
+                    <h1 className="font-Bai text-3xl font-bold text-center md:text-left my-2 md:my-0 md:text-2xl ">{topic ? topic : "All Posts"}</h1>
                 </div>
 
                 {posts.map(post => (
