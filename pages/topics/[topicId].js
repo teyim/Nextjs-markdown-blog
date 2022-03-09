4c11b24c756740c0fca892816463f884ae37653b
teyim <teyimasobo@gmail.com>
2022-03-09 20:47:14 +0200

    Create topics page

---
+++ pages/topics/[topicId].js
@@ -0,0 +1,11 @@
+import PostList from "../../components/posts/post-list"
+
+function Topics() {
+    return (
+        <div className='my-4 '>
+            <PostList posts={[]} topic="Vanilla js" />
+        </div>
+    )
+}
+
+export default Topics
