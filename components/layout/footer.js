807327e80688d6f8a0bfe0c3c35239a9f4ecd169
teyim <teyimasobo@gmail.com>
2022-03-13 17:10:13 +0200

    Update footer component

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,8 +1,8 @@
 import Link from 'next/link'
-
+import Form from '../form/form'
 function Footer() {
     return (
-        <footer className=" flex flex-col items-center bg-black w-full space-x-2 py-10 md:px-12 px-4 mt-8 text-white font-Rubik bottom-0">
+        <footer className=" flex flex-col items-center bg-black w-full space-x-2 py-10 md:px-12 px-4 mt-8 text-white font-Rubik bottom-0 ">
             <div className="md:flex w-full">
                 <div className="md:w-1/5 text-center ">
                     <span className="font-Bai font-extrabold text-2xl"><Link href='/' >Teyim A.</Link></span>
@@ -11,12 +11,8 @@ function Footer() {
                     </div>
                 </div>
                 <div className="md:w-1/2 py-2 flex flex-col items-center my-3 md:my-0">
-                    <h2 className="font-medium text-xl text-gray-300 my-2">Stay up to date with our newsletter</h2>
-                    <form className="flex flex-col md:w-2/3 items-center w-4/5">
-                        <input placeholder="First Name" type='text' className=" py-4 px-8 shadow-sm bg-gray-100 border rounded-xl my-2 text-gray-700 w-full"></input>
-                        <input placeholder="Email" type='email' className=" py-4 px-8 shadow-sm bg-gray-100 border rounded-xl my-2 text-gray-700 w-full"></input>
-                        <button type="submit" className=" font-Bai my-3 border md:w-1/2 py-2 px-4 bg-white font-semibold rounded-xl text-lg text-black hover:ring-white-300 active:ring-white-300 focus:outline-none focus:ring focus:ring-white-300 ">Sign me up!!</button>
-                    </form>
+                    <h2 className="font-medium text-xl text-gray-300 my-2">Subscribe to our newsletter</h2>
+                    <Form />
                 </div>
                 <div className="flex flex-col items-center md:items-end md:w-1/5 p-2  ">
                     <h2 className="font-medium text-xl text-gray-300 my-2">Links</h2>
