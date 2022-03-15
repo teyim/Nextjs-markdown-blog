1edb85ae5cdc8f9c00ec7e59a91a08f5eb638107
teyim <teyimasobo@gmail.com>
2022-03-15 17:37:54 +0200

    Modify single topic page

--- pages/topics/[topicId].js
+++ pages/topics/[topicId].js
@@ -1,11 +1,33 @@
+import { useRouter } from "next/router";
 import PostList from "../../components/posts/post-list"
+import { getPostByTopic } from "../../helpers/posts-utils";
 
-function Topics() {
+function Topics({ data }) {
+    const router = useRouter();
+    const { topicId } = router.query;
     return (
         <div className='my-4 '>
-            <PostList posts={[]} topic="Vanilla js" />
+            <PostList posts={data} heading={topicId} />
         </div>
     )
+}
+
+export const getStaticProps = (context) => {
+    const { params } = context;
+    const { topicId } = params
+    const posts = getPostByTopic(topicId)
+    return {
+        props: {
+            data: posts
+        },
+    }
+}
+export const getStaticPaths = () => {
+    const topics = ['react-js', 'css', 'next-js', 'typescript', 'vanilla-js', 'react-native']
+    return {
+        paths: topics.map((topic) => ({ params: { topicId: topic } })),
+        fallback: false
+    }
 }
 
 export default Topics
