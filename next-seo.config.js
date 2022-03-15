6e55eb4f94dd1324d94f4dd5a25f59f4a651b4b3
teyim <teyimasobo@gmail.com>
2022-03-15 19:18:13 +0200

    Create next-seo config

---
+++ next-seo.config.js
@@ -0,0 +1,16 @@
+const seoConfig = {
+    title: 'Markdownblog',
+    description: 'Get amazing well explained coding articles,which are easy to understand.list of articles exist on react,react native,css,nextjs and tailwind css',
+    openGraph: {
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://www.url.ie/',
+        site_name: 'SiteName',
+    },
+    twitter: {
+        handle: '@asofex',
+        site: '@site',
+        cardType: 'summary_large_image',
+    },
+};
+export default seoConfig;
